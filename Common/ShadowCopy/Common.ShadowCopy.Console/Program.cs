867602da58502761947b16f7e65e490c2017824f
eladmatari <eladmatari@gmail.com>
2021-06-30 00:57:11 +0300

    .

---
+++ Common/ShadowCopy/Common.ShadowCopy.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Common.ShadowCopy.Logic;
+using Common.ShadowCopy.Logic.Helpers;
+using Common.Utils.Standard;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.ShadowCopy.ConsoleNs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config.Init((key) =>
+            {
+                return ConfigurationManager.AppSettings[key];
+            }, new AppLogger());
+            SiteWatcher.Start();
+            Console.ReadKey();
+            SiteWatcher.Stop();
+        }
+    }
+}
