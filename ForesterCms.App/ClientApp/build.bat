415dd88e0854c650807c8afb0a515349a006a241
eladmatari <eladmatari@gmail.com>
2021-12-09 16:47:09 +0200

    .

--- ForesterCms.App/ClientApp/build.bat
+++
@@ -1,2 +0,0 @@
-cls
-webpack --config webpack.config.js
--- ForesterCms.App/ClientApp/webpack.config.prod.js
+++ ForesterCms.App/ClientApp/webpack.config.prod.js
@@ -1,9 +1,19 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let webpackConfig = require('./webpack.config.js');
+const hjson = require('hjson');
 
 webpackConfig.mode = 'production';
 webpackConfig.output.path = path.resolve(__dirname, '..', 'wwwroot', 'webpack', 'prod');
 webpackConfig.devtool = false;
+
+webpackConfig.plugins.forEach(function (val, ind, arr) {
+    if (val.definitions && val.definitions.__VUE_OPTIONS_API__ !== undefined) {
+        val.definitions.__VUE_OPTIONS_API__ = false;
+        val.definitions.__VUE_PROD_DEVTOOLS__ = false;
+    }
+});
+
+//console.log(hjson.stringify(webpackConfig.plugins));
 
 module.exports = webpackConfig;
