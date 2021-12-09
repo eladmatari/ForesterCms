15dd2dd9b11206a85564d9cca6c49a7f1191ff24
eladmatari <eladmatari@gmail.com>
2021-12-09 11:44:19 +0200

    .

--- ForesterCms.App/Startup.cs
+++ ForesterCms.App/Startup.cs
@@ -27,6 +27,7 @@ namespace ForesterCms.App
 {
     public class Startup
     {
+        // hello newman
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
