104785d3efeb4c0744140c3f8b2ff5c488e9c19a
eladmatari <eladmatari@gmail.com>
2022-01-18 00:20:03 +0200

    .

--- ForesterCms.App/ClientApp/src/modules/cms/branches/nav/nav.vue
+++ ForesterCms.App/ClientApp/src/modules/cms/branches/nav/nav.vue
@@ -16,6 +16,14 @@
                 var lsData = this.$root.getLocalStorage();
                 lsData.mainBranchId = this.$root.mainBranch.objId;
                 this.$root.setLocalStorage(lsData);
+            },
+            getEditLink() {
+                if (!this.$root.mainBranch)
+                    return '';
+
+                let branch = this.$root.mainBranch;
+
+                return '/ForesterCms/branch/edit/?b=' + branch.objId;
             }
         },
         created: function () {
@@ -25,13 +33,14 @@
 </script>
 
 <template>
-    <div>
-        <div>
+    <div class="nav">
+        <div class="nav-roots">
             <select v-model="$root.mainBranch" v-on:change="onTreeBranchChange()">
                 <option v-for="branchesTree in branchesTrees" :key="branchesTree.objId" :value="branchesTree">
                     {{ branchesTree.name }}
                 </option>
             </select>
+            <router-link :to="getEditLink()" class="mu mu-opts-v"></router-link>
         </div>
         <div>
             <cms-branches-items :branch="$root.mainBranch" v-if="$root.mainBranch"></cms-branches-items>
@@ -39,6 +48,30 @@
     </div>
 </template>
 
-<style>
-    
+<style scoped lang="scss">
+    .nav-roots {
+        display: flex;
+
+        a {
+            display: block;
+            width: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid #ccc;
+            border-right: none;
+            text-decoration: none;
+            color: transparent;
+        }
+
+        &:hover {
+            a {
+                color: #273849;
+
+                &:hover {
+                    color: gray;
+                }
+            }
+        }
+    }
 </style>
